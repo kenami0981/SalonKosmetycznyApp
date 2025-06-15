c748238deb76dcee6de03dab31ce4e747a6d9ad1
wsluzalek <sluzalek.weronika@gmail.com>
2025-06-15 21:20:26 +0200

    .

---
+++ SalonKosmetycznyApp/Model/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonKosmetycznyApp.Model
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int ProductStock {  get; set; }
+
+    }
+
+    public Product(int id, string name, string description, float price, int productStock)
+        {
+            Id = id;    
+            Name = name;
+            Description = description;
+            Price = price;
+            ProductStock = productStock;
+        }
+}
